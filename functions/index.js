7846db6a7248a6a701d119c4e47a0333eb61207a
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-22 11:28:57 -0400

    added update

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 import functions from  "firebase-functions"
 import express from "express"
-import {createCustomer} from './src/customers.js'
+import {createCustomer, getAllCustomers, updateCustomer} from './src/customers.js'
 
 const app = express()
 app.use(express.json())
 
 app.post('/customers', createCustomer)
+app.get('/customers', getAllCustomers)
+app.patch('/customers/:uid', updateCustomer)
 
 app.get('/test', (req, res) => res.send('Our API works!'))
 export const api = functions.https.onRequest(app)
--- functions/src/customers.js
+++ functions/src/customers.js
@@ -7,14 +7,22 @@ export async function createCustomer(req, res) {
     res.status(201).send({success: true, message: 'Customer Created ' + doc.id})
 }
 
-export async function getAllCutsomers(req, res) {
+export async function getAllCustomers(req, res) {
     const db = dbConnect()
     const collection = await db.collection('customers').get()
     .catch(err => res.status(500).send({ success: false, message: err}))
-    const users = collection.docs.map(doc => {
+    const customers = collection.docs.map(doc => {
         let customer = doc.data()
-        cutsomer.uid = doc.id
+        customer.uid = doc.id
         return customer
     })
-    res.send(customer)
+    res.send(customers)
+}
+
+export async function updateCustomer(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('customers').doc(uid).update(req.body)
+    .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({success: true, message: 'Clothing Updated' + doc.id })
 }

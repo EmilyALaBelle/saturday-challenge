e5a72cceeed9df558c570a03c0ce9099967f76b5
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-22 10:02:17 -0400

    created

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
-const functions = require("firebase-functions");
+import functions from  "firebase-functions"
+import express from "express"
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+app.use(express.json())
+
+app.get('/test', (req, res) => res.send('Our API works!'))
+export const api = functions.https.onRequest(app)
